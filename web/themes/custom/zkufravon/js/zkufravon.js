8f4116211aea260ddf0b732da5a946c21ebbff84
Peter Lachky <petiar@petiar.sk>
2023-10-16 22:10:42 +0200

    .gitignore refreshed.

---
+++ web/themes/custom/zkufravon/js/zkufravon.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Z kufra von behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.zkufravon = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
